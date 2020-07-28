e22a41a011f163e4ff3c3512687e979d1907f208
eopXD <yueh.ting.chen@gmail.com>
2020-07-29 00:44:09 +0800

    copy complete

--- 2020quiz2/Bstring.h
+++ 2020quiz2/Bstring.h
@@ -10,8 +10,17 @@ typedef struct Bstring {
 static inline bool bs_is_ptr ( const Bstring *bs ) {
 	return xs_is_ptr(&bs->x);
 }
+static inline bool bs_is_refer ( const Bstring *bs ) {
+	return xs_is_refer(&bs->x);
+}
+static inline bool bs_is_src ( const Bstring *bs ) {
+	return xs_is_src(&bs->x);
+}
 static inline char* bs_data ( Bstring *bs ) {
 	return xs_data(&bs->x);
+}
+static inline size_t bs_size ( const Bstring *bs ) {
+	return xs_size(&bs->x);
 }
 
 /* allocate/free */
@@ -25,8 +34,35 @@ void bs_free ( Bstring *bs ) {
 	xs_free(&bs->x);
 }
 
+/* reference counting */
+int ref_add ( Bstring *bs ) {
+	if ( bs_is_src(bs) ) {
+		*(bs->ref_count) += 1;
+	} else {
+		bs->ref_count = (int *)malloc(sizeof(int));
+		*(bs->ref_count) = 1;
+		bs->x.is_src = true;
+	}
+	return *(bs->ref_count);
+}
+
 /* basic function */
-//void bs_copy ( Bstring *src, Bstring *dest );
+Bstring* bs_copy ( Bstring *dest, Bstring *src ) {
+	if ( bs_is_ptr(src) ) {
+		dest->x.is_ptr = true;
+		dest->x.is_refer = true;
+		dest->x.ptr = src->x.ptr;
+		dest->x.size = bs_size(src);
+		ref_add(src);
+		dest->ref_count = src->ref_count;
+	} else {
+		dest->x.is_ptr = false;
+		memcpy(bs_data(dest), bs_data(src), bs_size(src));
+		dest->x.space_left = 15 - bs_size(src);
+	}
+	return dest;
+}
+
 //void bs_concat ( Bstring *bs, Bstring *prefix, Bstring *suffix );
 //void bs_trim ( Bstring *bs, const char *trimset );
 
--- 2020quiz2/main.c
+++ 2020quiz2/main.c
@@ -6,21 +6,28 @@ int main()
 {
     Bstring none;
     bs_new(&none, "");
-    printf("%d, %s\n", bs_is_ptr(&none), bs_data(&none));
-    bs_free(&none);
-    puts("free none");
+    printf("%d, %d, %s\n", bs_is_ptr(&none), bs_is_refer(&none), bs_data(&none));
     
     Bstring short_str;
     bs_new(&short_str, "short string");
-    printf("%d, %s\n", bs_is_ptr(&short_str), bs_data(&short_str));
-    bs_free(&short_str);
-    puts("free short");
+    printf("%d, %d, %s\n", bs_is_ptr(&short_str), bs_is_refer(&short_str), bs_data(&short_str));
     
     Bstring long_str;
     bs_new(&long_str, "a long enough string");
-    printf("%d, %s\n", bs_is_ptr(&long_str), bs_data(&long_str));
-    bs_free(&long_str);
-    puts("free long");
+    printf("%d, %d, %s\n", bs_is_ptr(&long_str), bs_is_refer(&long_str), bs_data(&long_str));
     
+    Bstring copy_short;
+    bs_copy(&copy_short, &short_str);
+    printf("%d, %d, %s\n", bs_is_ptr(&copy_short), bs_is_refer(&copy_short), bs_data(&copy_short));
+    
+    Bstring copy_long;
+    bs_copy(&copy_long, &long_str);
+    printf("%d, %d, %s\n", bs_is_ptr(&copy_long), bs_is_refer(&copy_long), bs_data(&copy_long));
+    
+    bs_free(&none);
+    bs_free(&short_str);
+    bs_free(&long_str);
+    bs_free(&copy_short);
+    bs_free(&copy_long);
     return 0;
 }
