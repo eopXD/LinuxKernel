21b5271800e7890c869c22b70e393dd6795df8cc
eopXD <yueh.ting.chen@gmail.com>
2020-07-30 15:41:39 +0800

    test for bs_copy

--- 2020quiz2/main.c
+++ 2020quiz2/main.c
@@ -14,7 +14,7 @@ int main()
     
     Bstring long_str;
     bs_new(&long_str, "a long enough string");
-    printf("%d, %d, %s\n", bs_is_ptr(&long_str), bs_is_refer(&long_str), bs_data(&long_str));
+    printf("%d, %d, %s, %zu\n", bs_is_ptr(&long_str), bs_is_refer(&long_str), bs_data(&long_str), bs_ref_count(&long_str));
     
     Bstring copy_short;
     bs_copy(&copy_short, &short_str);
@@ -22,12 +22,22 @@ int main()
     
     Bstring copy_long;
     bs_copy(&copy_long, &long_str);
-    printf("%d, %d, %s\n", bs_is_ptr(&copy_long), bs_is_refer(&copy_long), bs_data(&copy_long));
+    printf("%d, %d, %s, %zu\n", bs_is_ptr(&copy_long), bs_is_refer(&copy_long), bs_data(&copy_long), bs_ref_count(&copy_long));
+    bs_free(&long_str);
+    printf("%d, %d, %s, %zu\n", bs_is_ptr(&copy_long), bs_is_refer(&copy_long), bs_data(&copy_long), bs_ref_count(&copy_long));
+
+    Bstring copy_copy;
+    bs_copy(&copy_copy, &copy_long);
+    //bs_free(&copy_long);
+    printf("%d, %d, %s, %zu\n", bs_is_ptr(&copy_copy), bs_is_refer(&copy_copy), bs_data(&copy_copy), bs_ref_count(&copy_copy));
     
+    bs_free(&copy_copy);
+    
+    printf("%d, %d, %s, %zu\n", bs_is_ptr(&copy_long), bs_is_refer(&copy_long), bs_data(&copy_long), bs_ref_count(&copy_long));
+    bs_free(&copy_long);
+
     bs_free(&none);
     bs_free(&short_str);
-    bs_free(&long_str);
     bs_free(&copy_short);
-    bs_free(&copy_long);
     return 0;
 }
