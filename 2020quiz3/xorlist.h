8bf2087aa25896443bdd537e83d78e607edf982c
eopXD <yueh.ting.chen@gmail.com>
2020-08-05 16:02:58 +0800

    increase readibility, add traversal

--- 2020quiz3/xorlist.h
+++ 2020quiz3/xorlist.h
@@ -1,16 +1,32 @@
 // XOR Linked-List
+// main drawback: unable to delete element inside list
 #include <stdint.h>
 #include <stdlib.h>
 
 typedef struct __list list;
 struct __list {
     int data;
-    struct __list *addr; // link = addr_prev XOR addr_next
+    struct __list *addr; // link = addr_self XOR addr_next
 };
+
+
+/*
+
+
+NULL A NULL
+
+insert B, given ADDR_A, so we also have ADDR_B
+ADDR_B = ADDR_A
+ADDR_A = ADDR_B XOR ADDR_A 
+
+
+*/
 
 #define XOR(a, b) ((list *) ((uintptr_t) (a) ^ (uintptr_t) (b)))
 
-void insert_node(list **l, int d) {
+#define prev(a) ((list *) ((uintptr_t) ))
+
+void insert_head(list **l, int d) { // insert to head
     list *tmp = malloc(sizeof(list));
     tmp->data = d;
 
@@ -31,4 +47,17 @@ void delete_list(list *l) {
         free(l);
         l = next;
     }
+}
+
+void print_list ( list *l ) {
+    list *now = l, *prev = NULL, *next;
+
+    while ( now ) {
+        printf("%d ", now->data);
+        
+        next = XOR(prev, now->addr);
+        prev = now;
+        now = next;
+    }
+    puts("");
 }
